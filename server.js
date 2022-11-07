e9c3f64acb73a1145bdcb686a46f72420db03c8c
sagharnoursobhi <saghar.noursobhi@hyperisland.se>
2022-11-07 12:40:53 +0100

    Creating and testing first invoice

--- server.js
+++ server.js
@@ -2,11 +2,36 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 //create static web server
 //first argument specifies the path to directories that contains static files */
 //path.join(__dirname, 'dist)__dirname contains the path to the root directory and dist is a directory contains static files
 app.use(express.static(path.join(__dirname, 'dist')));
+
+
+//connection to mongodb
+mongoose.connect('mongodb+srv://saghar2330:admin2330@'
++ 'invoicegeneratorapp.gf0bvy0.mongodb.net/invoiceStorage?'
++ 'retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Error: ${err}`);
+})
+
+//if everything is working
+mongoose.connection.once('open', () => {
+    console.log('The connection to MongoDB Atlas is working')
+})
+
+
+//body-parser configuration
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //create rest api routes
 //the second argument is the javascript file that will be run after this route is used.
