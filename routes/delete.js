a3f98ffb68e40b5d485dd8b579f31064d15fe565
Sagharnoursobhi <76443044+sagharnoursobhi@users.noreply.github.com>
2022-11-08 17:13:50 +0100

    Delete routes directory

--- routes/delete.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.get('/', (request, response) => {
-    response.send('<h1>Hello from router delete.js</h1>')
-})
-
-module.exports = router;
--- routes/read.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (request, response) => {
-    response.send('<h1>Hello from router read.js</h1>')
-})
-
-module.exports = router;
